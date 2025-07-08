2f52f54441e088b4da0cc50d7c1caeb718ccde41
Shamikh <shamikhshaikh07@gmail.com>
2025-07-09 01:34:33 +0500

    Initial commit

---
+++ DBMS/main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const routes = require('./route/routes');
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', routes);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
